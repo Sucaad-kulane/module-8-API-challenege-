bc1e4fe7ebebdb40d44d82e2d0dbd303d9c36f8c
sucad hassan <sucad1@hotmail.co.uk>
2023-02-05 18:26:23 +0000

    added API link

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 
+const weatherAPIURL = "https://api.openweathermap.org"
+const weatherAPIKey = "bf20521a7eb62dbd6a15e97075ca1eae"
 
 let searchInput = $("#search-input")
 let searchForm = $("#search-form");
 
 
+function fetchCoord(search){
+    let queryURL = `${weatherAPIURL}/geo/1.0/direct?q={search}`
+
+}
 function submitSearchForm(event){
 
     event.preventDefult();
 
 
-    alert(searchInput.val().trim())
+    let search = searchInput.val().trim()
+
+    fetchCoord (search);
 }
 
 
