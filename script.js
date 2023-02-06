df511f9743ba02fb71582db661090a99a0b8cbb3
sucad hassan <sucad1@hotmail.co.uk>
2023-02-06 08:14:05 +0000

    created a function for searchHistory

--- script.js
+++ script.js
@@ -1,60 +1,69 @@
 
 const weatherAPIURL = "https://api.openweathermap.org"
 const weatherAPIKey = "bf20521a7eb62dbd6a15e97075ca1eae"
-let searchHistory = [ ]
+let searchHistory = []
 
 let searchInput = $("#search-input")
 let searchForm = $("#search-form");
-let searchHistoryContainer =$("#history")
+let searchHistoryContainer = $("#history")
 
 
+function renderSearchHistory() {
+    searchHistoryContainer.htm("")
 
-function fetchCoord(search){
+    for (let i = 0; i < searchHistory.length; i++) {
+        let btn = $("<button>");
+        btn.attr("type", "button")
+        btn.addClass("history-btn btn-hitso")
+
+        btn.attr("data-search", searchHistory[i])
+        btn.text(searchHistory[i])
+
+        searchHistoryContainer.append(btn)
+    }
+
+}
+
+function appendSearchHistory() {
+    if (searchHistory.indexOf(search) !== -1) {
+        return
+    }
+    searchHistory.push(search);
+
+    localStorage.setItem("search-history", JSON.stringify(search));
+    renderSearchHistory()
+
+}
+
+
+function fetchCoord(search) {
     let queryURL = `${weatherAPIURL}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherAPIKey}`;
     console.log(queryURL);
-    fetch(queryURL, { method: "GET"}).then(function(data){
+    fetch(queryURL, { method: "GET" }).then(function (data) {
         return data.json()
-    }).then (function(response){
-        if(!response[0]){
+    }).then(function (response) {
+        if (!response[0]) {
             alert("Response not found")
-        }else{
-            if(searchHistory.indexOf(search) !== -1){
-                return
-            }
-            searchHistory.push(search);
-
-            localStorage.setItem("search-history", JSON.stringify(search));
-            
-            searchHistoryContainer.htm("")
-
-            for(let i = 0; i < searchHistory.length; i++){
-                let btn = $("<button>");
-                btn.attr("type","button")
-                btn.addClass("history-btn btn-hitso")
-
-
-                btn.attr("data-search", searchHistory[i])
-                searchHistoryContainer.append(btn)
-            }
-
+        } else {
+            renderSearchHistory()
 
         }
-      
+
     })
 
 }
-function submitSearchForm(event){
+function submitSearchForm(event) {
 
     event.preventDefult();
 
 
     let search = searchInput.val().trim()
 
-    fetchCoord (search);
+    fetchCoord(search);
 }
 
 
-searchForm.on("submit",submitSearchForm);
+searchForm.on("submit", submitSearchForm);
 
 
 
